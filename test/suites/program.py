de7348923838f7b3ab93380a8e9536457959831f
dan-nyanko <dnyanko@dtec.com>
2015-08-20 16:31:37 -0400

    adding test suites for runner.py

---
+++ test/suites/program.py
@@ -0,0 +1,18 @@
+import os
+import unittest
+
+from tools.grits_consumers import GritsConsumer
+
+_SCRIPT_DIR = os.path.dirname(__file__)
+
+class TestGritsConsumer(unittest.TestCase):
+    def setUp(self):
+        self.cmd = GritsConsumer()
+        self.validFile = open(os.path.join(_SCRIPT_DIR, '../data/Schedule_Weekly_Extract_Report.tsv'))
+        self.invalidFile = open(os.path.join(_SCRIPT_DIR, '../data/Schedule_Weekly_Extract_Report.csv'))
+
+    def test_is_valid_extension(self):
+        self.assertEqual(True, self.cmd.is_valid_file_type(self.validFile))
+    
+    def test_is_invalid_extension(self):
+        self.assertRaises(Exception, self.cmd.is_valid_file_type(self.invalidFile))

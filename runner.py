867d1816b43ef5c566f57c4a312dba3b90e122ac
dan-nyanko <dnyanko@dtec.com>
2015-08-21 11:58:57 -0400

    moved the test runner, added vendor.add('lib')

---
+++ runner.py
@@ -0,0 +1,14 @@
+import unittest
+import vendor
+vendor.add('lib')
+
+import test.suites.program
+
+test_modules = [test.suites.program]
+
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    for module in test_modules:
+        suite.addTests(unittest.defaultTestLoader.loadTestsFromModule(module))
+    runner = unittest.TextTestRunner()
+    runner.run(suite)

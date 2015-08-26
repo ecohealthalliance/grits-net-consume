d5f814436a138c33d2fa93718a04bc46092c8923
dan-nyanko <dnyanko@dtec.com>
2015-08-26 11:29:51 -0400

    wrote additional tests for grits_record.py

--- tests/test__tools_grits_record.py
+++ tests/test__tools_grits_record.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+import math
 
 from tools.grits_record import Record, FlightRecord, AirportRecord
 from tools.grits_record import InvalidRecordProperty, InvalidRecordLength
@@ -8,17 +9,77 @@ _SCRIPT_DIR = os.path.dirname(__file__)
 
 class TestGritsRecord(unittest.TestCase):
     def setUp(self):
-        self.row = [u'1', u'2', u'3', u'4']
         self.obj = Record()
-
-    def test_create_without_headers(self):
-        self.assertRaises(InvalidRecordProperty, self.obj.create, self.row)
-    
-    def test_validate_empty(self):
-        self.assertRaises(InvalidRecordLength, self.obj.validate_required)
     
     def test_to_json_empty(self):
         self.assertEqual("{}", self.obj.to_json())
+    
+    def test_is_empty_string(self):
+        self.assertEqual(True, Record.is_empty_str(""))
+        self.assertEqual(True, Record.is_empty_str(u""))
+        self.assertEqual(False, Record.is_empty_str(u"adfasdf"))
+        self.assertEqual(False, Record.is_empty_str(1234123))
+        self.assertEqual(False, Record.is_empty_str(None))
+    
+    def test_could_be_boolean(self):
+        self.assertEqual(True, Record.could_be_boolean("1"))
+        self.assertEqual(True, Record.could_be_boolean(1))
+        self.assertEqual(True, Record.could_be_boolean(True))
+        self.assertEqual(True, Record.could_be_boolean(False))
+        self.assertEqual(True, Record.could_be_boolean(0))
+        self.assertEqual(True, Record.could_be_boolean("1"))
+        self.assertEqual(False, Record.could_be_boolean("ABCD"))
+        self.assertEqual(False, Record.could_be_boolean(1234))
+        self.assertEqual(False, Record.could_be_boolean(None))
+    
+    def test_could_be_float(self):
+        self.assertEqual(True, Record.could_be_float("1"))
+        self.assertEqual(True, Record.could_be_float(u"1.1"))
+        self.assertEqual(True, Record.could_be_float("0.1"))
+        self.assertEqual(True, Record.could_be_float("000000000000000000000000.1"))
+        self.assertEqual(True, Record.could_be_float(0000000000000000000000000.1))
+        self.assertEqual(True, Record.could_be_float(10000000000000000000000000.1))
+        self.assertEqual(True, Record.could_be_float(str(math.pow(2,63)-1)))
+        self.assertEqual(False, Record.could_be_float(10000000000000000000000000))
+        self.assertEqual(False, Record.could_be_float("abcd"))
+        self.assertEqual(False, Record.could_be_float(None))
+        self.assertEqual(False, Record.could_be_float("1111.10000000000.000000000"))
+    
+    def test_could_be_int(self):
+        self.assertEqual(True, Record.could_be_int("1"))
+        self.assertEqual(True, Record.could_be_int(0))
+        self.assertEqual(True, Record.could_be_int(2147483647))
+        self.assertEqual(True, Record.could_be_int(-2147483647))
+        self.assertEqual(True, Record.could_be_int("-2147483647"))
+        self.assertEqual(False, Record.could_be_int(math.pow(2,32)))
+        self.assertEqual(False, Record.could_be_int(u"1.1"))
+        self.assertEqual(False, Record.could_be_int("0.1"))
+        self.assertEqual(False, Record.could_be_int("000000000000000000000000.1"))
+        self.assertEqual(False, Record.could_be_int(0000000000000000000000000.1))
+        self.assertEqual(False, Record.could_be_int(10000000000000000000000000))
+        self.assertEqual(False, Record.could_be_int("abcd"))
+        self.assertEqual(False, Record.could_be_int(None))
+        self.assertEqual(False, Record.could_be_int("1111.10000000000.000000000"))
+    
+    def test_could_be_number(self):
+        self.assertEqual(True, Record.could_be_number("1"))
+        self.assertEqual(True, Record.could_be_number("1.1"))
+        self.assertEqual(True, Record.could_be_number("0.1"))
+        self.assertEqual(True, Record.could_be_number("000000000000000000000000.1"))
+        self.assertEqual(True, Record.could_be_number(0000000000000000000000000.1))
+        self.assertEqual(True, Record.could_be_number(10000000000000000000000000.1))
+        self.assertEqual(True, Record.could_be_number(str(math.pow(2,63)-1)))
+        self.assertEqual(True, Record.could_be_number(10000000000000000000000000))
+        self.assertEqual(False, Record.could_be_number("abcd"))
+        self.assertEqual(False, Record.could_be_number(None))
+        self.assertEqual(False, Record.could_be_number("1111.10000000000.000000000"))
+    
+    def test_could_be_datetime(self):
+        self.assertEqual(True, Record.could_be_datetime("Jan 2014", '%b %Y'))
+        self.assertEqual(True, Record.could_be_datetime(u"September 1, 2014", '%B %m, %Y'))
+        self.assertEqual(False, Record.could_be_datetime(None, None))
+        self.assertEqual(False, Record.could_be_datetime("", ""))
+        self.assertEqual(False, Record.could_be_datetime("Jan 2004", ""))
 
 class TestGritsFlightRecord(unittest.TestCase):
     def setUp(self):
@@ -38,15 +99,19 @@ class TestGritsFlightRecord(unittest.TestCase):
         self.assertRaises(InvalidRecordProperty, self.invalid_obj.create, self.row)
     
     def test_validate_invalid_obj(self):
-        self.assertRaises(InvalidRecordLength, self.invalid_obj.validate_required)
+        self.assertEquals(False, self.invalid_obj.validate())
     
     def test_create_valid_obj(self):
         self.valid_obj.create(self.row)
-        self.assertEqual(self.row, self.valid_obj.fields.values())
+        self.assertEqual(len(self.row), len(self.valid_obj.fields.values()))
     
     def test_create_valid_obj_invalid_row(self):
         row = []
         self.assertRaises(InvalidRecordLength, self.valid_obj.create, row)
+    
+    def test_create_valid_obj_validate(self):
+        self.valid_obj.create(self.row)
+        self.assertEqual(True, self.valid_obj.validate())
     
 class TestGritsAirportRecord(unittest.TestCase):
     def setUp(self):
@@ -56,6 +121,10 @@ class TestGritsAirportRecord(unittest.TestCase):
         self.row = [u'AAA', u'Anaa', u'Anaa', u'', u'', u'-17.351700',
                     u'-145.497800', u'PF', u'French Polynesia', u'Australasia',
                     u'823', u'']
+        # setting a required field to empty string
+        self.invalid_row = [u'', u'Anaa', u'Anaa', u'', u'', u'-17.351700',
+                    u'-145.497800', u'PF', u'French Polynesia', u'Australasia',
+                    u'823', u'']
         self.valid_obj = AirportRecord(self.headers)
         self.invalid_obj = AirportRecord(None)
     
@@ -63,17 +132,13 @@ class TestGritsAirportRecord(unittest.TestCase):
         self.assertRaises(InvalidRecordProperty, self.invalid_obj.create, self.row)
     
     def test_validate_invalid_obj(self):
-        self.assertRaises(InvalidRecordLength, self.invalid_obj.validate_required)
-    
-    def test_create_valid_obj_valid_row(self):
-        self.valid_obj.create(self.row)
-        self.assertEqual(self.row, self.valid_obj.fields.values())
+        self.assertEquals(False, self.invalid_obj.validate())
     
     def test_create_valid_obj_invalid_row(self):
-        row = []
-        self.assertRaises(InvalidRecordLength, self.valid_obj.create, row)
+        self.valid_obj.create(self.invalid_row)
+        self.assertEquals(False, self.valid_obj.validate())
     
-    def test_create_valid_obj_vaidate_required(self):
+    def test_create_valid_obj_validate_required(self):
         self.valid_obj.create(self.row)
-        self.assertEqual(True, self.valid_obj.validate_required())
+        self.assertEquals(True, self.valid_obj.validate())
     

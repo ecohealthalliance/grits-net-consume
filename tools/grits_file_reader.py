253b0a9c6cb88cc2fb2e9ef4cd222be973df36c6
dan-nyanko <dnyanko@dtec.com>
2015-08-28 13:08:11 -0400

    additional documentation, constructor accepts mongo_connection as arg

--- tools/grits_file_reader.py
+++ tools/grits_file_reader.py
@@ -13,9 +13,12 @@ class InvalidFileFormat(Exception):
         super(InvalidFileFormat, self).__init__(message)
 
 class GritsFileReader:
-    def __init__(self, record_type, program_arguments):
+    """ the class responsible for reading the file"""
+    def __init__(self, record_type, program_arguments, mongo_connection):
         self.type= record_type
         self.arguments = program_arguments
+        
+        self.mongo_connection = mongo_connection
         
         self.records = set() #collection of records
         self.invalid_records = set() #collection of invalid records
@@ -26,6 +29,7 @@ class GritsFileReader:
         self.reader = UnicodeReader(program_arguments.infile, dialect=self.type.dialect)
     
     def process_file(self):
+        """ iterate over each row in the file"""
         try:
             row_count = 0
             for row in self.reader:
@@ -35,6 +39,7 @@ class GritsFileReader:
             logging.error("file %r, line %d, %r" % (self.validFile.name, self.reader.line_num, e))
     
     def process_row(self, row_count, row):
+        """ process each row according to the record type contract """
         if self.arguments.verbose:
             print '%r' % row
             pass
@@ -54,7 +59,7 @@ class GritsFileReader:
         if row_count > self.type.data_position and not self.end_of_data:
             if any(row):
                 headers = self.header.keys() # in order
-                record = self.type.record(headers)
+                record = self.type.record(headers, self.type.collection_name, self.mongo_connection)
                 record.create(row)
                 if record.validate():
                     self.records.add(record)
@@ -62,7 +67,6 @@ class GritsFileReader:
                     invalid_record = InvalidRecord(datetime.utcnow(), record.validation_errors(), type(record).__name__, row_count)
                     if invalid_record.validate():
                         self.invalid_records.add(invalid_record)
-                    #logging.error("record.error: %r, %r, %d", record.validation_errors(), record.record_type, row_count)
             else:
                 if self.type.num_empty_rows_eod > 0:
                     self.empty_row_count += 1

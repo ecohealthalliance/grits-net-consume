9544c3107dc8e5069675e10e05b17d06af83d9e5
dan-nyanko <dnyanko@dtec.com>
2015-09-11 12:27:11 -0400

    added comma dialect and remove the writer class

--- tools/csv_helpers.py
+++ tools/csv_helpers.py
@@ -14,6 +14,18 @@ class TabDialect(csv.Dialect):
         self.quoting = csv.QUOTE_MINIMAL
         csv.Dialect.__init__(self)
 
+class CommaDialect(csv.Dialect):
+    """ dialect for comma separated values """
+    def __init__(self):
+        self.delimiter = ','
+        self.quotechar = '"'
+        self.escapechar = None
+        self.doublequote = True
+        self.skipinitialspace = False
+        self.lineterminator = '\r\n'
+        self.quoting = csv.QUOTE_MINIMAL
+        csv.Dialect.__init__(self)
+
 """ unicode support from https://docs.python.org/2/library/csv.html#csv-examples """
 class UTF8Recoder:
     """
@@ -44,32 +56,3 @@ class UnicodeReader:
 
     def __iter__(self):
         return self
-
-class UnicodeWriter:
-    """
-    A CSV writer which will write rows to CSV file "f",
-    which is encoded in the given encoding.
-    """
-
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        # Redirect output to a queue
-        self.queue = cStringIO.StringIO()
-        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
-        self.stream = f
-        self.encoder = codecs.getincrementalencoder(encoding)()
-
-    def writerow(self, row):
-        self.writer.writerow([s.encode("utf-8") for s in row])
-        # Fetch UTF-8 output from the queue ...
-        data = self.queue.getvalue()
-        data = data.decode("utf-8")
-        # ... and reencode it into the target encoding
-        data = self.encoder.encode(data)
-        # write to the target stream
-        self.stream.write(data)
-        # empty queue
-        self.queue.truncate(0)
-
-    def writerows(self, rows):
-        for row in rows:
-            self.writerow(row)

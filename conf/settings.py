5a67a899dadcfe627e474952096dac5445ebb396
dan-nyanko <dnyanko@dtec.com>
2015-09-11 12:10:22 -0400

    added settings for _THREADING_ENABLED, _CHUNK_SIZE, and _NODES

--- conf/settings.py
+++ conf/settings.py
@@ -9,8 +9,8 @@ if (_DEBUG):
     logging.getLogger().setLevel(logging.DEBUG)
 
 # command-line options
-_ALLOWED_FILE_EXTENSIONS = ['.tsv']
-_TYPES = ['airport', 'flight']
+_ALLOWED_FILE_EXTENSIONS = ['.tsv','.csv']
+_TYPES = ['DiioAirport', 'FlightGlobal']
 
 # default strftime format for a records date field
 _STRFTIME_FORMAT = '%b %Y'
@@ -19,3 +19,13 @@ _STRFTIME_FORMAT = '%b %Y'
 _AIRPORT_COLLECTION_NAME = 'airports'
 _FLIGHT_COLLECTION_NAME = 'flights'
 _INVALID_RECORD_COLLECTION_NAME = 'invalidRecords'
+
+# schema
+_DISABLE_SCHEMA_MATCH = True #raise exception for headers not in the schema?
+
+# number of lines to split the file
+_CHUNK_SIZE = 5000
+
+# threading?
+_NODES = 5
+_THREADING_ENABLED = True

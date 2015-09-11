c15f12503a006b98a5059322550eff295e41bd7f
dan-nyanko <dnyanko@dtec.com>
2015-09-11 12:29:53 -0400

    added batch processing and async threading support

--- tools/grits_file_reader.py
+++ tools/grits_file_reader.py
@@ -1,9 +1,13 @@
 import csv
 import logging
 import collections
+import time
+import sys
 
+from pathos.threading import ThreadPool
 from datetime import datetime
 
+from conf import settings
 from tools.grits_record import InvalidRecord
 from tools.csv_helpers import UnicodeReader
 
@@ -11,7 +15,7 @@ class InvalidFileFormat(Exception):
     """ custom exception that is thrown when the file format is invalid """
     def __init__(self, message, *args, **kwargs):
         """ InvalidFileFormat constructor
-            
+
             Parameters
             ----------
                 message : str
@@ -21,92 +25,138 @@ class InvalidFileFormat(Exception):
 
 class GritsFileReader:
     """ the class responsible for reading the file """
-    def __init__(self, record_type, program_arguments, mongo_connection):
+
+    def __init__(self, provider_type, program_arguments):
         """ GritsFileReader constructor
-            
+
             Parameters
             ----------
-                record_type : object
-                    A record type object from grits_record.py
-                    {FlightRecord, AirportRecord}
+                provider_type : object
+                    A provider type object from grits_provider_type.py
                 program_arguments: dict
                     A dict containing the argparse program arguments
-                mongo_connection: object
-                    A GritsMongoConnection object from grits_mongo.py
+
         """
-        self.type= record_type
-        self.arguments = program_arguments
-        
-        self.mongo_connection = mongo_connection
-        
-        self.records = set() #collection of records
-        self.invalid_records = set() #collection of invalid records
-        
+
+        self.provider_type = provider_type
+        self.program_arguments = program_arguments
+
         self.empty_row_count = 0 # number of empty rows encountered within record set
         self.end_of_data = False # flag that represents that the end of the data has been reached
-        self.header = collections.OrderedDict() # ordered dict to hold the fields of each data row
-        self.reader = UnicodeReader(program_arguments.infile, dialect=self.type.dialect)
-    
-    def process_file(self):
-        """ iterate over each row in the file"""
-        try:
-            row_count = 0
-            for row in self.reader:
-                self.process_row(row_count, row)
-                row_count += 1
-        except csv.Error, e:
-            logging.error("file %r, line %d, %r" % (self.validFile.name, self.reader.line_num, e))
-    
-    def process_row(self, row_count, row):
+        self.header_row = []
+
+    @staticmethod
+    def gen_chunks(reader, mongo_connection):
+        """ yield chunks of the file for batch processing """
+        chunk = [];
+        for row_number, row in enumerate(reader):
+            if (row_number % settings._CHUNK_SIZE == 0 and row_number > 0):
+                yield chunk
+                del chunk[:]
+            chunk.append([row_number, row, mongo_connection])
+        yield chunk
+
+    def find_header(self, reader):
+        """ find the header based off the provider_type """
+        for row_number, line in enumerate(reader):
+            if row_number == self.provider_type.header_position:
+                # store the report header and continue
+                if any(field.strip() for field in line):
+                    for field in line:
+                        self.header_row.append(field.strip().lower())
+                else:
+                    raise InvalidFileFormat("header at position [%d] is empty" % row_number)
+
+                break
+
+    def process(self, mongo_connection):
+        """ process a chunk of rows in the file """
+        reader = UnicodeReader(self.program_arguments.infile, dialect=self.provider_type.dialect)
+        self.find_header(reader)
+
+        for chunk in GritsFileReader.gen_chunks(reader, mongo_connection):
+            # collections of valid and invaid records to be batch upsert / insert many
+            valid_records = []
+            invalid_records = []
+            # is threading enabled?  this may increase performance when mongoDB
+            # is not running on localhost due to busy wait on finding an airport
+            # in the case of FlightGlobalType.
+            if settings._THREADING_ENABLED:
+                pool = ThreadPool(nodes=settings._NODES)
+                results = pool.amap(self.process_row, chunk)
+
+                while not results.ready():
+                    # command-line spinner
+                    for cursor in '|/-\\':
+                        sys.stdout.write('\b%s' % cursor)
+                        sys.stdout.flush()
+                        time.sleep(.25)
+
+                sys.stdout.write('\b')
+                sys.stdout.flush()
+                # async-poll is done, get the results
+                result = results.get()
+                valid_records = [ x[0] for x in result if x[0] is not None ]
+                invalid_records = [ x[1] for x in result if x[1] is not None ]
+
+            else:
+                # single-threaded synchronous processing
+                for data in chunk:
+                    valid, invalid = self.process_row(data)
+                    if valid != None: valid_records.append(valid)
+                    if invalid != None: invalid_records.append(invalid)
+
+            # bulk upsert / inset many of the records
+            valid_result = mongo_connection.bulk_upsert(self.provider_type.collection_name, valid_records)
+            invalid_result = mongo_connection.insert_many(settings._INVALID_RECORD_COLLECTION_NAME, invalid_records)
+            logging.debug('valid_result: %r', valid_result)
+            logging.debug('invalid_result: %r', invalid_result)
+
+    def process_row(self, args):
         """ process each row according to the record type contract
-            
+
             Parameters
             ----------
-                row_count : int
+                args[0] - row_count : int
                     the current row number
-                row: object
+                args[1] - row: object
                     A python csv module row object
+                args[2] - mongo_connection: object
+                    A GritsMongoConnection object from grits_mongo.py
         """
-        if self.arguments.verbose:
+        row_count = args[0]
+        row = args[1]
+        mongo_connection = args[2]
+
+        if self.program_arguments.verbose:
             # echo the contents of the row in verbose mode
             print '%r' % row
-            pass
-        if row_count == self.type.title_position:
-            # store the report title and continue
-            if any(field.strip() for field in row):
-                self.title = row
-            else:
-                raise InvalidFileFormat("title at position [%d] is empty" % row_count)
-            return
-        if row_count == self.type.header_position:
-            # store the report header and continue
-            if any(field.strip() for field in row):
-                for field in row:
-                    self.header[field.strip()] = None
-            else:
-                raise InvalidFileFormat("header at position [%d] is empty" % row_count)
-            return
-        if row_count > self.type.data_position and not self.end_of_data:
-            # process the record set
+
+        if row_count >= self.provider_type.data_position and not self.end_of_data:
             if any(row):
-                headers = self.header.keys() # get the header columns in order
+                header_row = self.header_row
+                provider_map = self.provider_type.map
+                collection_name = self.provider_type.collection_name
+
                 # init the record object based on the type
-                record = self.type.record(headers, self.type.collection_name, self.mongo_connection)
-                # create the fields based on the current row
+                record = self.provider_type.record(header_row, provider_map, collection_name, row_count, mongo_connection)
+
+                # create the record
                 record.create(row)
-                # validate against the records schema
+
+                # validate
                 if record.validate():
-                    # add to the set of valid records for later bulk upsert
-                    self.records.add(record)
+                    return [record,None]
                 else:
-                    # create an invalid record
-                    invalid_record = InvalidRecord(datetime.utcnow(), record.validation_errors(), type(record).__name__, row_count)
+                    invalid_record = InvalidRecord(datetime.utcnow(), record.validation_errors(), type(record).__name__, record.row_count)
                     if invalid_record.validate():
-                        # add to the set of invalid records for bulk insert
-                        self.invalid_records.add(invalid_record)
+                        return [None,invalid_record]
             else:
                 # check for special case where empty line signal end_of_data
-                if self.type.num_empty_rows_eod > 0:
+                if self.provider_type.num_empty_rows_eod > 0:
                     self.empty_row_count += 1
-                    if self.empty_row_count >= self.type.num_empty_rows_eod:
+                    logging.debug('empty_row_count: %d', empty_row_count)
+                    if self.empty_row_count >= self.provider_type.num_empty_rows_eod:
                         self.end_of_data = True
+
+        return [None,None]

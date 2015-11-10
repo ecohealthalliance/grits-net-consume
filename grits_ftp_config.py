21f90547a0c69ad3fd1736df5fd2ac818217da85
Brock Arnold <arnold@ecohealthalliance.org>
2015-11-09 20:59:21 -0500

    Script to pull flight down new flight data automatically, unzips zip file, md5 digest

---
+++ grits_ftp_config.py
@@ -0,0 +1,6 @@
+# Modify this so we can connect to the Innovata deliverable flight data.
+# Currently they support just plain vanilla FTP access
+
+url = 'url-innovata.com'
+uname = 'username'
+pwd = 'password'

a60dbc19bed51f368ae0808e6265689ce1f2e611
dan-nyanko <dnyanko@dtec.com>
2015-11-12 13:54:46 -0500

    python script will load latest flight data by default:
    - conf/settings.py, now has variable _DATA_DIR for the location of the
    FTP script result
    - grits_consume.py, how will load the latest flight data generated by
    grits_ftp_pull
    - tools/grits_consumer.py will check for *args prior to using command-
    line args

--- grits_consume.py
+++ grits_consume.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
+import os
+import sys
+import glob
 
+from conf import settings
 from tools.grits_consumer import GritsConsumer
 
-""" wrapper for running GritsConsumer """
 
+""" wrapper for running GritsConsumer """
 if __name__ == '__main__':
+    
     cmd = GritsConsumer()
-    cmd.run()
+    
+    def get_lastest_csv():
+        """ get the most recent filename, sorted by date with extension .csv """
+        data_dir = os.path.join(os.getcwd() + settings._DATA_DIR)
+        return max(glob.iglob(data_dir + '*.[Cc][Ss][Vv]'), key=os.path.getctime)
+    
+    if len(sys.argv) <= 1:
+        lastest_csv = get_lastest_csv()
+        cmd.run('--type', 'FlightGlobal', lastest_csv)
+    else:
+        cmd.run()

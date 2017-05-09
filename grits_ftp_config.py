6ab02e10c4d886addf696ab0c1a64dd0f688d893
Abe Miessler <miessler@ecohealthalliance.org>
2017-05-09 08:47:46 -0700

    remove password from code.  Pass in through jenkins

--- grits_ftp_config.py
+++ grits_ftp_config.py
@@ -1,6 +1,6 @@
 # Modify this so we can connect to the Innovata deliverable flight data.
 # Currently they support just plain vanilla FTP access
 
-url = 'suwweb03.innovata-llc.com'
-uname = 'ECOHEALTH'
-pwd = 'HD38jFXc'
+url = 'ftp'
+uname = 'usename'
+pwd = 'password'
